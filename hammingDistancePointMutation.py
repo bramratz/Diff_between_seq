294f7d67484537a6ff6369af312c6cd89f4d9337
Bram Ratz <bratz@uoguelph.ca>
2020-06-08 16:40:51 -0400

    Program to calculate the hamming distance of two sequences, s and t, to determine the number of point mutations that have likely occured as a number of differences betweent eh two

---
+++ hammingDistancePointMutation.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Given two strings of equal length, s and t, will calculate the 
+hamming distance for s and t. That is it will find the number
+of symbols the two sequences differ by. The number of differences 
+correpond to the number of point mutations that have occured
+between the two sequences.
+ 
+Created on Mon Jun  8 16:36:22 2020
+
+@author: bram
+"""
+
