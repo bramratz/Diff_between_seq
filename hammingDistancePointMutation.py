77230bac3191695e2f14922fa19af9b29ee925a6
Bram Ratz <bratz@uoguelph.ca>
2020-06-08 16:57:14 -0400

    Added sys arguments and main loop to compare lists to find differences between two sequences, s and t

--- hammingDistancePointMutation.py
+++ hammingDistancePointMutation.py
@@ -12,3 +12,22 @@ Created on Mon Jun  8 16:36:22 2020
 @author: bram
 """
 
+# Import modules 
+import sys
+
+# Save strings to be compared as variables 
+s = list(sys.argv[1])
+t = list(sys.argv[2])
+
+# Initialize counter for the number of differences betwween the two strings 
+diffCounter = 0
+
+# Iterate both lists and compare elements to one another
+for val1, val2 in zip(s, t):
+    if val1 == val2:
+        continue # If elements are the same do nothing 
+    else:
+        diffCounter += 1
+
+# Print result to terminal 
+print(f'hamming distance between s and t is: {diffCounter}')
